bb9ad8edc6bab588ee911610d9c00d1a3cfcf8ad
quhairfoir <moragstc@gmail.com>
2018-09-11 17:31:55 +0000

    finished up to exercise 4

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,18 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+
+@store1 = Store.find_by(id: 1)
+
+
+puts @store1.inspect
+
+@store1.name = "Saanich"
+@store1.save
+
+puts @store1.inspect
+
+@store2 = Store.find_by(id: 2)
+
+puts @store2.inspect
+
--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,16 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+store4 = Store.create(name: "Oak Bay", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+store5 = Store.create(name: "Fairfield", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+store6 = Store.create(name: "Gordon Head", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+@mens_stores.each do |store|
+  puts "#{store.name}: $#{store.annual_revenue} per year."
+end
+
+@failing_womens_apparel = Store.where(womens_apparel: true, annual_revenue: (0..500000))
+@failing_womens_apparel.each do |store|
+  puts "#{store.name}: $#{store.annual_revenue} per year."
+end
