1337ba0c3b84daba18d45b56fe34c94dbd043b34
quhairfoir <moragstc@gmail.com>
2018-09-11 17:43:57 +0000

    exercise 5 finished

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,6 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+puts "Total income of stores: #{Store.sum(:annual_revenue)}"
+puts "Average income: #{Store.average(:annual_revenue).round}"
+puts "There are #{Store.where(annual_revenue: (1000000..Float::INFINITY)).count} stores that make +$1mil a year"
